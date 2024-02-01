140e7827643c990d0d677aacea0d8dc76b6f9db9
Jonslogar Arenque <jon.a@cynsolutions.ph>
2024-02-01 15:53:21 +0800

    initial commit for develop template.

---
+++ src/GloboAsiatico01Plugin.js
@@ -0,0 +1,94 @@
+import React from 'react';
+import { FlexPlugin } from '@twilio/flex-plugin';
+
+import CustomTaskList from './components/CustomTaskList/CustomTaskList';
+
+import globoLogo from './assets/globo-asiatico-logo.jpg';
+import cynLogo from './assets/cyn-logo.jpeg';
+
+const PLUGIN_NAME = 'GloboAsiatico01Plugin';
+
+export default class GloboAsiatico01Plugin extends FlexPlugin {
+  constructor() {
+    super(PLUGIN_NAME);
+  }
+
+  /**
+   * This code is run when your plugin is being started
+   * Use this to modify any UI components or attach to the actions framework
+   *
+   * @param flex { typeof import('@twilio/flex-ui') }
+   */
+  async init(flex, manager) {
+    const options = { sortOrder: -1 };
+    // * Main Header
+    flex.MainHeader.Content.add(
+      <div
+        key="custom-header"
+        style={{
+          overflow: 'hidden',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          marginLeft: '10px',
+          marginRight: '-27px'
+        }}
+      >
+        <img
+          src={globoLogo}
+          alt="Globo Asiatico"
+          style={{ height: '30px', marginRight: '5px' }}
+        />
+        <p>Globo Asiatico Powered by</p>
+
+        <img
+          src={cynLogo}
+          alt="CYN Solutions Inc."
+          style={{ height: '30px', marginRight: '5px', marginLeft: '5px' }}
+        />
+        <p>CYN Solutions Inc. </p>
+        <p style={{ fontSize: '19px', marginLeft: '8px' }}>X</p>
+      </div>,
+      { sortOrder: 1 }
+    );
+
+    // * No Task String
+    manager.strings.NoTasks = 'No task right now, go make some coffee. :)';
+
+    // * Panel 1
+    flex.AgentDesktopView.Panel1.Content.add(
+      <CustomTaskList key="GloboAsiatico01Plugin-component" />,
+      options
+    );
+
+    // ? Panel 2: Removed CRM Side for now
+    flex.AgentDesktopView.defaultProps.showPanel2 = false;
+
+    // ? Inbound Call Phone number format
+    flex.Actions.replaceAction('StartOutboundCall', (payload, original) => {
+      // Default all outbound calls to external SIP interface
+      /*
+       * Instructions:
+       * Replace "sipInterfaceIPAddress" with external SIP Interface
+       */
+
+      const sipInterfaceIPAddress = '98.158.148.76';
+      const technicalprefix = '3953';
+      const termination = ';edge=tokyo';
+
+      console.log(`Before payload Destigation: ${payload.destination}`);
+
+      payload.destination =
+        'sip:' +
+        technicalprefix +
+        payload.destination +
+        `@${sipInterfaceIPAddress}` +
+        termination;
+      payload.callerId = '+639190599575';
+      console.log('updated outbound call to:', payload);
+      original(payload);
+    });
+
+    console.log('OUTSIDE updated outbound call to:', payload);
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import * as FlexPlugin from '@twilio/flex-plugin';
+
+import GloboAsiatico01Plugin from './GloboAsiatico01Plugin';
+
+FlexPlugin.loadPlugin(GloboAsiatico01Plugin);
